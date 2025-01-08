c0293ebc42a7e6233a054fe378c34c6842ef68dd
Nithin1729S <sureshnithin1729@gmail.com>
2025-01-08 20:46:01 +0530

    update

--- StrategyDesignPattern/MathOperation.cpp
+++ StrategyDesignPattern/MathOperation.cpp
@@ -4,7 +4,9 @@ using namespace std;
 class ImathOperation{
     public:
         virtual void operate(int x,int y)=0;
-        virtual ~ImathOperation(){};
+        virtual ~ImathOperation(){
+            cout<<"Deleted"<<endl;
+        };
 };
 
 class AddStratery:public ImathOperation{
@@ -42,17 +44,27 @@ class MathContext{
         MathContext(ImathOperation*obj){
             this->obj=obj;
         }
+        ~MathContext() {
+            delete obj;
+        }
         void setStrategy(ImathOperation*obj){
+            delete this->obj;
             this->obj=obj;
         }
         void operate(int x,int y){
             obj->operate(x,y);
+            
         }  
+
 };
 
 int main() {
     MathContext *op = new MathContext(new MultiplyStratery());
     op->operate(1, 10);
+    op->setStrategy(new AddStratery());
+    op->operate(1,10);
+    op->setStrategy(new DivideStratery());
+    op->operate(80,8);
     delete op;
     return 0;
 }
